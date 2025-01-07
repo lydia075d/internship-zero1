5cbf53f59144e2a208c4b7bb80c6c718c99c13c7
lydia075d <163287560+lydia075d@users.noreply.github.com>
2025-01-07 22:40:41 +0530

    Add files via upload

---
+++ task 3/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from './components/Navbar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
